a39079a8af09864efc63b0c0eb31a71e7f92a198
ashupednekar <ashutoshpednekar15@gmail.com>
2024-09-21 09:46:59 +0530

    chore: updated src

--- quickgrpc/src/rpc/employeemgmt.py
+++ quickgrpc/src/rpc/employeemgmt.py
@@ -5,48 +5,21 @@ DB: dict = {}
 
 class EmployeemgmtService(EmployeemgmtServicer):
     def create(self, employee_inp, context):
-        employee = {
-            "name": employee_inp.name,
-            "dept_id": employee_inp.dept_id,
-            "designation": employee_inp.designation
-        } 
-        DB[len(DB) + 1] = employee
-        return employeemgmt_pb2.Employee(
-            name=employee["name"],
-            dept_id=employee["dept_id"],
-            designation=employee["designation"]
+        employee = employeemgmt_pb2.Employee(
+            name=employee_inp.name,
+            dept_id=employee_inp.dept_id,
+            designation=employee_inp.designation
         )
+        DB[len(DB) + 1] = employee
+        return employee
 
     def list(self, _, context):
         res = employeemgmt_pb2.Employees()
-        res.employees.extend([employeemgmt_pb2.Employee(
-            name=entry["name"],
-            dept_id=entry["dept_id"],
-            designation=entry["designation"]
-        ) for entry in DB.values()])
+        res.employees.extend([employee for _, employee in DB.items()])
         return res
 
-    def update(self, employee_inp, context):
-        employee = {
-            "name": employee_inp.name,
-            "dept_id": employee_inp.dept_id,
-            "designation": employee_inp.designation
-        } 
-        #DB[] = employee
-        return employeemgmt_pb2.Employee(
-            name=employee["name"],
-            dept_id=employee["dept_id"],
-            designation=employee["designation"]
-        )
-
     def retrieve(self, employee_id, context):
-        employee = DB[employee_id.id]
-        return employeemgmt_pb2.Employee(
-            name=employee["name"],
-            dept_id=employee["dept_id"],
-            designation=employee["designation"]
-        )
-
+        return DB[employee_id.id]
 
     def delete(self, employee_id, context):
         del DB[employee_id.id]
--- quickgrpc/src/rpc/tests/test_employeemgmt.py
+++ quickgrpc/src/rpc/tests/test_employeemgmt.py
@@ -2,27 +2,28 @@ from quickgrpc.utils import get_stub
 import grpc_assets.employeemgmt.employeemgmt_pb2 as pb2
 from unittest import TestCase
 
+
 class EmployeemgmtClientTestCase(TestCase):
     stub = get_stub("employeemgmt")
 
-    def test_crud(self):
-        # stub.<your rpc method>(pb2.<your proto message>(params))
-        r1 = self.stub.create(pb2.EmployeeInp(
-            name="ashu",
-            dept_id=1,
-            designation="dev"
-        ))
-        print(f"created employee: {r1}")
-        r2 = self.stub.create(pb2.EmployeeInp(
+    def test_crd(self):
+        self.stub.create(pb2.EmployeeInp(
+                name="ashu",
+                dept_id=1,
+                designation="dev"
+            ))
+        print("created employee ashu")
+        self.stub.create(pb2.EmployeeInp(
             name="jane",
-            dept_id=1,
+            dept_id=2,
             designation="devops"
         ))
-        print(f"created employee: {r2}")
-        l = self.stub.list(pb2.Empty())
-        print(f"employee list\n: {l}")
-        print(self.stub.retrieve(pb2.EmployeeID(id=1)))
-        print(self.stub.delete(pb2.EmployeeID(id=1)))
-        print(self.stub.list(pb2.Empty()))
-
-   
+        print("created employee jane")
+        res = self.stub.list(pb2.Empty())
+        print(f"listing employees: {res}")
+        ashu = self.stub.retrieve(pb2.EmployeeID(id=1))
+        print(f"retrieved ashu: {ashu}")
+        self.stub.delete(pb2.EmployeeID(id=1))
+        print("deleted ashu")
+        res = self.stub.list(pb2.Empty())
+        print(f"listing employees: {res}")
