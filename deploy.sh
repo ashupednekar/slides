24de27783d651658bb5a758a8c629a5d78292f61
ashu <ashu@192.168.1.9>
2026-06-25 18:36:56 +0530

    chore: sync

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-cd "$repo_root"
-
-(cd render && go run .)
-
-wrangler pages deploy dist/ "$@"
+go install github.com/ashupednekar/slides/render@c2c1437d15dcce98607a8cf3a2660b8e9bb47541
+render
+wrangler pages deploy dist/ 
